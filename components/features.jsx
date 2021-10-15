060298498882c78d9f6730c3c5b94036735c92cf
Dam Van Nam <namdvse04360@gmail.com>
2021-10-15 17:21:34 +0700

    smooth scrolling + parallax + features layout

--- components/features.jsx
+++ components/features.jsx
@@ -1,8 +1,8 @@
 export const Features = (props) => {
   return (
-    <div id='features' className='text-center'>
-      <div className='container'>
-        <div className='col-md-10 col-md-offset-1 section-title'>
+    <div id="features" className="text-center">
+      <div className="container">
+        <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Gameplay</h2>
           <p>
             Himo World takes you to an epic battle, where you can challenge even
@@ -10,23 +10,30 @@ export const Features = (props) => {
             Tournament, Betting,... with monthly updates and worthy rewards.
           </p>
         </div>
-        <div className='row'>
-          <img
-            src='/static/img/gameplayscreen.jpg'
-            className='img-responsive'
-            alt=''
-            style={{ padding: 50 }}
-          />
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-md-4'>
-                  {' '}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : 'Loading...'}
+        <div className="row">
+          <div className="col-md-6">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="row"
+                    style={{ padding: 20 }}
+                  >
+                    <i className={d.icon}></i>
+                    <h3>{d.title}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+          <div className="col-md-6">
+            <img
+              src="/static/img/mockup.png"
+              className="img-responsive"
+              alt=""
+              style={{ padding: 10 }}
+            />
+          </div>
         </div>
       </div>
     </div>
