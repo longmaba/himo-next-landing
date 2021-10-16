07834a90f2ca9e1bba8c7f690cb4134b99de8691
longmaba <longmaba@gmail.com>
2021-10-16 22:48:49 +0700

    fix parallax and add orc image

--- components/header.jsx
+++ components/header.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 
 export const Header = (props) => {
   const introElement = useRef();
@@ -8,40 +8,42 @@ export const Header = (props) => {
     let _mouseX = e.clientX;
     let _mouseY = e.clientY;
 
-    let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${
-      50 - (_mouseY - _h) * 0.01
+    let _depth1 = `${50 - (_mouseX - _w) * 0.02}% ${
+      50 - (_mouseY - _h) * 0.02
     }%`;
-    let _depth2 = `${50 - (_mouseX - _w) * 0.01}% ${
-      50 - (_mouseY - _h) * 0.01
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
+      50 - (_mouseY - _h) * 0.02
     }%`;
-    let _depth3 = `50% ${50 - (_mouseY - _h) * 0.02}%`;
+    let _depth3 = `${50 - (_mouseX - _w) * -0.02}% ${
+      50 - (_mouseY - _h) * -0.04
+    }%`;
 
     let x = `${_depth3}, ${_depth2}, ${_depth1}`;
     introElement.current.style.backgroundPosition = x;
   };
 
   return (
-    <header id="header">
-      <div className="intro" onMouseMove={parallaxHandler} ref={introElement}>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 intro-text">
+    <header id='header'>
+      <div className='intro' onMouseMove={parallaxHandler} ref={introElement}>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-8 col-md-offset-2 intro-text'>
               <h1>
-                {props.data ? props.data.title : "Loading"}
+                {props.data ? props.data.title : 'Loading'}
                 <span></span>
               </h1>
               <p>
                 <mark
                   style={{
-                    backgroundColor: "#37194f",
-                    color: "white",
-                    fontWeight: "bold",
+                    backgroundColor: '#37194f',
+                    color: 'white',
+                    fontWeight: 'bold',
                   }}
                 >
-                  {props.data ? props.data.paragraph : "Loading"}
+                  {props.data ? props.data.paragraph : 'Loading'}
                 </mark>
               </p>
-              <a href="#about" className="btn btn-custom btn-lg page-scroll">
+              <a href='#about' className='btn btn-custom btn-lg page-scroll'>
                 Learn More
               </a>
             </div>
