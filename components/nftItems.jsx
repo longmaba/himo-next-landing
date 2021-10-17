8eb99f25d66a654018f32a11f3aa0fc6c9220ac4
Dam Van Nam <namdvse04360@gmail.com>
2021-10-17 09:55:42 +0700

    Carousel

--- components/nftItems.jsx
+++ components/nftItems.jsx
@@ -1,21 +1,54 @@
+import Slider from 'react-slick';
+
 export const NFTItems = (props) => {
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          autoplay: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
           <h2>NFT Items</h2>
           <p>
-            We have original/ground-breaking new NFT model. Our model only
-            consists of single ERC-20 token and the rest are NFT.
+            We have original/ground-breaking new NFT model. Our model only consists of single ERC-20
+            token and the rest are NFT.
           </p>
         </div>
-        <div className="row">
-          <img
+        {/* <img
             src="/static/img/NFTItems.png"
             style={{ width: "100%" }}
             alt=""
-          />
-        </div>
+          /> */}
+        <Slider {...settings}>
+          <div>
+            <img src="/static/img/nft_1.png" style={{ width: '100%' }} alt="" />
+          </div>
+          <div>
+            <img src="/static/img/nft_2.png" style={{ width: '100%' }} alt="" />
+          </div>
+          <div>
+            <img src="/static/img/nft_3.png" style={{ width: '100%' }} alt="" />
+          </div>
+          <div>
+            <img src="/static/img/nft_2.png" style={{ width: '100%' }} alt="" />
+          </div>
+        </Slider>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/index.css';
 import '../styles/App.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
