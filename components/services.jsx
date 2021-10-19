b0b747977961b058fd6cc6a9dcc12b7f5dead26d
Dam Van Nam <namdvse04360@gmail.com>
2021-10-19 14:33:42 +0700

    page logo + update service icons

--- components/services.jsx
+++ components/services.jsx
@@ -5,24 +5,26 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>What is $HIMO token?</h2>
           <p>
-            Powered by Binance Smart Chain (BSC), $HIMO will be our main
-            resources in the game. In traditional games, this could be
-            gem/diamond, resources that the player has to purchase IAP to have
-            or super hard to get.
+            Powered by Binance Smart Chain (BSC), $HIMO will be our main resources in the game. In
+            traditional games, this could be gem/diamond, resources that the player has to purchase
+            IAP to have or super hard to get.
           </p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  <i className={d.icon}></i>
+                  {/* <i className={d.icon}></i> */}
+                  <div className="service-logo-wrapper">
+                    <img className="service-logo" src={d.img} alt="" />
+                  </div>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
                   </div>
                 </div>
               ))
-            : "loading"}
+            : 'loading'}
         </div>
       </div>
     </div>
