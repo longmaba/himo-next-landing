965df95d4b4395b23679d666a5828f4184c4358e
longmaba <longmaba@gmail.com>
2021-11-14 13:13:42 +0700

    add whitepaper link

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,54 +1,55 @@
 export const Contact = (props) => {
-
   return (
     <div>
-      <div id='contact'>
-        <div className='container'>
-          <div className='col-md-8'>
-            <div className='social'>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="social">
               <ul>
                 <li>
-                  <a href={props.data ? props.data.facebook : '/'}>
-                    <i className='fa fa-facebook'></i>
+                  <a href={props.data ? props.data.medium : '/'}>
+                    <i className="fa fa-medium"></i>
                   </a>
                 </li>
                 <li>
                   <a href={props.data ? props.data.twitter : '/'}>
-                    <i className='fa fa-twitter'></i>
+                    <i className="fa fa-twitter"></i>
                   </a>
                 </li>
                 <li>
-                  <a href={props.data ? props.data.youtube : '/'}>
-                    <i className='fa fa-youtube'></i>
+                  <a href={props.data ? props.data.telegram : '/'}>
+                    <i className="fa fa-telegram"></i>
                   </a>
                 </li>
               </ul>
-            </div><div className='row'>
             </div>
+            <div className="row"></div>
           </div>
-          <div className='col-md-3 col-md-offset-1 contact-info'>
-            <div className='contact-item'>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
               <h3>Contact Info</h3>
               <p>
                 <span>
-                  <i className='fa fa-map-marker'></i> Address
+                  <i className="fa fa-map-marker"></i> Address
                 </span>
                 {props.data ? props.data.address : 'loading'}
               </p>
             </div>
-            <div className='contact-item'>
+            <div className="contact-item">
               <p>
                 <span>
-                  <i className='fa fa-envelope-o'></i> Email
+                  <i className="fa fa-envelope-o"></i> Email
                 </span>{' '}
-                {props.data ? <a href={`mailto:${props.data.email}`}>{props.data.email}</a> : 'loading'}
+                {props.data ? (
+                  <a href={`mailto:${props.data.email}`}>{props.data.email}</a>
+                ) : (
+                  'loading'
+                )}
               </p>
             </div>
           </div>
-          <div className='col-md-12'>
-            <div className='row'>
-
-            </div>
+          <div className="col-md-12">
+            <div className="row"></div>
           </div>
         </div>
       </div>
